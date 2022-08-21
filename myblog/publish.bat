92c01b1e06479280963b1118cb2fd2d65cb34793
shixiong2333 <50489744+shixiong2333@users.noreply.github.com>
2022-08-21 23:53:54 +0800

    change

--- myblog/publish.bat
+++ myblog/publish.bat
@@ -1,3 +1,10 @@
+copy public\CNAME CNAME
+copy public\_config.yml _config.yml
 del /q public\*.*
 for /f "delims=" %%a in ('dir /ad/s/b public') do rd /s/q "%%~a"
+
+copy CNAME public\CNAME
+copy _config.yml public\_config.yml
+del CNAME
+del _config.yml
 hugo
